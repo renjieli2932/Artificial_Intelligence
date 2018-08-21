194af0a9a8532232481054e67d85301c070e3f8a
Renjie Li <jimmyleeyog@gmail.com>
2018-08-21 00:14:55 -0400

    Update
    Routine Update, Not Started Yet

---
+++ 2048(W4)/PlayerAI.py
@@ -0,0 +1,12 @@
+# Artifical Intelligence @ edX
+# Week4 Project
+# Adversial Search and Games : 2048
+# Renjie Li, rl2932@columbia.edu
+
+from random import randint
+from BaseAI import BaseAI
+
+class PlayerAI(BaseAI):
+    def getMove(self, grid):
+        moves = grid.getAvailableMoves()
+        return moves[randint(0, len(moves) - 1)] if moves else None
